ad45f3abb9cb6452f8b8b2aec01dc8256d394deb
SergeySKRV <ap0llyon@yandex.ru>
2026-05-06 16:18:56 +0300

    Initialize DiaryApp Xcode project with UIKit and Core Data
    Add generated project structure and initial app files

---
+++ DiaryApp/DiaryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiaryApp
+//
+//  Created by Сергей Скориков on 06.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
