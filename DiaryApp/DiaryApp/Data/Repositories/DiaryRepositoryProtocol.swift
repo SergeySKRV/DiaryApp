4eb6a1427b78c2d6641574e27c7402c035cd6a68
SergeySKRV <ap0llyon@yandex.ru>
2026-05-06 16:29:44 +0300

    Organize Xcode project structure into app core data domain and modules
    Add base folders for services resources and feature modules
    
    Add initial scaffolding files for diary app architecture

---
+++ DiaryApp/DiaryApp/Data/Repositories/DiaryRepositoryProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  DiaryRepositoryProtocol.swift
+//  DiaryApp
+//
+//  Created by Сергей Скориков on 06.05.2026.
+//
+
+import Foundation
---
+++ DiaryApp/DiaryApp/Domain/Models/DiaryEntryModel.swift
@@ -0,0 +1,8 @@
+//
+//  DiaryEntryModel.swift
+//  DiaryApp
+//
+//  Created by Сергей Скориков on 06.05.2026.
+//
+
+import Foundation
